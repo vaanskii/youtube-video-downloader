e6a3f387d2727ae4f8851607630d2cd8c8c19138
vaanskii <giorgivanadze03@gmail.com>
2024-03-19 22:26:01 +0400

    Added progress tracking and UI updates during download. This commit introduces changes to enhance the user experience during the download process. The following updates were implemented:
    1. Added progress tracking: The  function now tracks the download progress of the video and updates the progress bar and percentage label accordingly.
    
    2. Updated  function: The  parameter was added to the  object constructor to enable progress tracking during download. Additionally, the ]2;]1; label now displays the title of the video being downloaded.
    
    3. Added UI elements: Two new UI elements were introduced -  to display download completion status and  to show the download progress percentage.
    
    4. Enhanced error handling: Improved error handling in the  function to display appropriate messages in case of download errors.
    
    These changes provide real-time feedback to the user during the download process, enhancing the usability and transparency of the application.

--- main.py
+++ main.py
@@ -11,12 +11,25 @@ from pytube import YouTube
 def startDownload():
     try:
         ytLink = link.get()
-        ytObject = YouTube(ytLink)
+        ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         video = ytObject.streams.get_highest_resolution()
+        title.configure(text=ytObject.title, text_color='white')
+        finishLabel.configure(text="")
         video.download()
+        finishLabel.configure(text='Downloaded!')
     except:
-        print("Invalid link!")
-    print('Download complete!')
+        finishLabel.configure(text='Download error!', text_color='red')
+
+def on_progress(stream, chunk, bytes_remaining):
+    total_size = stream.filesize
+    bytes_downloaded = total_size - bytes_remaining
+    percentage_of_completion = bytes_downloaded / total_size * 100
+    per = str(int(percentage_of_completion))
+    pPercentage.configure(text=per + '%')
+    pPercentage.update()
+
+    progressBar.set(float(percentage_of_completion) / 100)
+
 
 # System settings
 customtkinter.set_appearance_mode('System')
@@ -35,6 +48,18 @@ title.pack(padx=10, pady=10)
 url_var = tkinter.StringVar()
 link = customtkinter.CTkEntry(app, width=350, height=40, textvariable=url_var)
 link.pack()
+
+# Finished downloading
+finishLabel = customtkinter.CTkLabel(app, text="")
+finishLabel.pack()
+
+# Progress percentage
+pPercentage = customtkinter.CTkLabel(app, text='0%')
+pPercentage.pack()
+
+progressBar = customtkinter.CTkProgressBar(app, width=400)
+progressBar.set(0)
+progressBar.pack(padx=10, pady=10)
 
 # Download button
 download = customtkinter.CTkButton(app, text='Download', command=startDownload)
